231206d95c5ac293d7ae6299eec8a5368ce3a7f7
Greg <2000247C@student.tp.edu.sg>
2022-04-06 18:57:12 +0800

    fixed a small bug with the images

--- greensg-custom-theme/custom-sections/section-navbar.php
+++ greensg-custom-theme/custom-sections/section-navbar.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-md-3 d-flex justify-content-center">
-        <img class="logo" src="<?php echo get_template_directory_uri(); ?> /img/greensgcollab-logo-outlined.png">
+        <img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/greensgcollab-logo-outlined.png">
       </div>
 
       <div class="col-md-6">
