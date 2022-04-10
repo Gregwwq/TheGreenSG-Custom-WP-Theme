d084f4ced6a7969f43e509e610c1c580827a3cd1
Zacharia Aslam <s10204144@connect.np.edu.sg>
2022-04-10 21:43:51 +0800

    Updated styling for webinars page

--- greensg-custom-theme/custom-templates/template-ourwebinars.php
+++ greensg-custom-theme/custom-templates/template-ourwebinars.php
@@ -28,7 +28,53 @@ Template Name: Our Webinars
       sunt in culpa qui officia deserunt mollit anim id est laborum
     </div>
   </div>
+  <article class="postcard">
+			
+				<img class="webinar-img" src="<?php echo get_template_directory_uri(); ?>/img/webinars/webinar1.png">
+			
+			<div class="postcard__text t-dark">
+				<h1 class="postcard__title blue"><a href="#">Fashion X Sustainability</a></h1>
+				<div class="postcard__subtitle small">
+					<time datetime="2020-05-25 12:00:00">
+						<i class="fas fa-calendar-alt mr-2"></i> Date: 31 March 2022 <br>
+              Time: 8.00PM to 9.30PM <br>
+              Venue: Zoom Meeting<br> 
+					</time>
+				</div>
+				<div class="postcard__bar"></div>
+				<div class="postcard__preview-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</div>
+        <br>
+        <br>
+        <br>
+        <div class="reg-btn-wrapper">
+              <button type="button" class="reg-btn">Register</button>
+            </div>
+			</div>
+		</article>
 
+    <!-- WEBINAR 2-->
+    <article class="postcard">
+				<img class="webinar-img" src="<?php echo get_template_directory_uri(); ?>/img/webinars/webinar2.jpg">
+			<div class="postcard__text t-dark">
+				<h1 class="postcard__title blue"><a href="#"> Graduating Into The Green Workforce</a></h1>
+				<div class="postcard__subtitle small">
+					<time datetime="2020-05-25 12:00:00">
+						<i class="fas fa-calendar-alt mr-2"></i> Date: 13 Oct 2021 <br>
+              Time: 7.30PM to 9.00PM <br>
+              Venue: Zoom Meeting <br> 
+					</time>
+				</div>
+				<div class="postcard__bar"></div>
+				<div class="postcard__preview-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</div>
+        <br>
+        <br>
+        <br>
+        <div class="reg-btn-wrapper">
+              <button type="button" class="reg-btn">Register</button>
+            </div>
+			</div>
+		</article>
+<!--
   <div class="webinar-container">
     <div class="row webinar-row">
       <div class="col-7">
@@ -58,7 +104,8 @@ Template Name: Our Webinars
       </div>
     </div>
   </div>
-
+-->
+<!--
   <div class="webinar-container">
     <div class="row webinar-row">
       <div class="col-7">
@@ -88,7 +135,7 @@ Template Name: Our Webinars
       </div>
     </div>
   </div>
-
+-->
   <!--END CODE-->
 </body>
 <?php get_footer(); ?>
