0922d3a5b4f1d525893afe5e6a35cafb7c6a8467
Greg <2000247C@student.tp.edu.sg>
2022-04-21 16:18:23 +0800

    linked community activities more details button to page

--- greensg-custom-theme/custom-sections/section-actcommunity.php
+++ greensg-custom-theme/custom-sections/section-actcommunity.php
@@ -5,7 +5,7 @@
         <h1>Activities With The Community</h1>
         These are the activities organized by The GreenSG that aims to enrich the community with information about living sustainability.
         <div class="more-btn-wrap">
-          <button class="more-btn" type="button">More Details &#8594;</button>
+          <button class="more-btn" type="button" onclick="location.href=location.href+'/community-activities';">More Details &#8594;</button>
         </div>
       </div>
     </div>
